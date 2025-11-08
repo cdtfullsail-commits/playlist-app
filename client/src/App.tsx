0d2173901238a4f7dd8c80e2f0bd35a9eb161fd4
OG BEATZ <admin@iMac.local>
2025-11-08 03:54:10 -0800

    ✅ Added full routing and React entrypoint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,54 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import AdminUpload from "./components/AdminUpload";
-import PlaylistView from "./components/PlaylistView";
-import PlaylistEditor from "./components/PlaylistEditor";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Login";
-import { isLoggedIn } from "./auth";
-
-const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-  return isLoggedIn() ? children : <Navigate to="/login" />;
-};
+import AdminDashboard from "./components/AdminDashboard";
+import AdminUpload from "./components/AdminUpload";
+import PlaylistEditor from "./components/PlaylistEditor";
+import PlaylistView from "./components/PlaylistView";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<PlaylistView />} />
-        <Route path="/login" element={<Login />} />
-
-        <Route
-          path="/admin"
-          element={
-            <ProtectedRoute>
-              <AdminUpload />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/edit"
-          element={
-            <ProtectedRoute>
-              <PlaylistEditor />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/admin" element={<AdminDashboard />} />
+      <Route path="/upload" element={<AdminUpload />} />
+      <Route path="/edit" element={<PlaylistEditor />} />
+      <Route path="/playlist" element={<PlaylistView />} />
+    </Routes>
   );
 };
 
 export default App;
-import AdminDashboard from './components/AdminDashboard';
-// ...
-<Route
-  path="/admin"
-  element={
-    <ProtectedRoute>
-      <AdminDashboard />
-    </ProtectedRoute>
-  }
-/>
